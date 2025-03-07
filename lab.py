49f58e3d1b5e3ea59cec9eedd25bedc84cafd9eb
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-03-07 13:43:22 +1000

    Create lab.py

---
+++ lab.py
@@ -0,0 +1,18 @@
+#x = int(input("Введите x (0 | 1): "))
+#y = int(input("Введите y (0 | 1): "))
+
+#and_res = x and y
+#or_res = x or y
+#not_x = not x
+#not_y = not y
+#implic = not x or y
+#equivalence = x == y
+#xor = x != y
+
+#print(f"x and y: {int(and_res)}")
+#print(f"x or y: {int(or_res)}")
+#print(f"not x: {int(not_x)}")
+#print(f"not y: {int(not_y)}")
+#print(f"x → y: {int(implic)}")
+#print(f"x ≡ y: {int(equivalence)}")
+#print(f"x ∨ y: {int(xor)}")
